0ef29edc45c14b3cd66866742026a3e5206377e5
EloiseSchachterle <eloises3@nycstudents.net>
2026-01-12 15:33:34 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Sign a =  new Sign("abcdefghijk", 2);
+        Sign a =  new Sign("abcdefghij", 2);
         a.getLines();
         System.out.println();
         System.out.println(a.numberOfLines());
+        String str;
+        Sign sign1 = new Sign("ABC222DE", 3);
+        sign1.getLines();
+        System.out.println();
+        Sign sign2 = new Sign("ABCD", 10);
+        sign2.getLines();
+        Sign sign3 = new Sign("ABCDEF", 6);
+        System.out.println(sign3.numberOfLines());
+        sign3.getLines();
+
     }
 }
