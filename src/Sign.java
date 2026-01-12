5540026eaa9aa33e79034b49d32a48f5cddb184e
EloiseSchachterle <eloises3@nycstudents.net>
2026-01-12 15:21:48 -0500

    Update Sign.java

--- src/Sign.java
+++ src/Sign.java
@@ -10,11 +10,15 @@ public class Sign {
     }
     public void getLines(){
         int num = numberOfLines();
-        for (int i = 0; i < num; i++){
+        int x = 0;
+        for (int i = 0; i < num - 1; i++){
             int k = width * i;
             System.out.print(text.substring(k, k + width) + ";");
+            x = k;
         }
+        if(text.length() % 2 == 0){
+        System.out.println(text.substring(x + width));}
+        else System.out.println(text.substring(x + width, x + width + 2) + ";" + text.substring(x + width + 2));
 
     }
 }
-
