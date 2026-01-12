12c53c6b69d8f8165ffaf441ece1a71257aa00d3
EloiseSchachterle <eloises3@nycstudents.net>
2026-01-12 15:00:58 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Sign a =  new Sign("abcdefghijk", 2);
+        a.getLines();
+        System.out.println();
+        System.out.println(a.numberOfLines());
     }
 }
